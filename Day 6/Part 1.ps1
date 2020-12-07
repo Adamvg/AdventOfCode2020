3a2622fdddb56bccfb9ef5065140dc1aeb144a25
Adamvg <vangelder.adam@gmail.com>
2020-12-07 20:19:41 +0000

    Completed Part 1

---
+++ Day 6/Part 1.ps1
@@ -0,0 +1,31 @@
+# get input data as string
+[string]$inputData = Get-Content .\Input.txt
+
+# reset variables
+$yesCount = 0
+
+# split into groups
+[array]$groups = $inputData -split '  '
+
+# iterate through groups
+Foreach($answers in $groups){
+    
+    # split into individual people's answer
+    $individualAnswers = $answers -split ' '
+
+    # reset TempArray for every below foreach
+    $answerArrayTemp = @()
+
+    # iterate through individual answers
+    Foreach($individualAnswer in $individualAnswers){
+        # run through each letter
+        for($i=0; $i -lt $individualAnswer.Length; $i++){
+            if(!($answerArrayTemp -contains $individualAnswer[$i])){$answerArrayTemp += $individualAnswer[$i]}
+        }
+    }
+    
+    # add to count
+    $yesCount += $answerArrayTemp.Count
+}
+
+Return $yesCount
